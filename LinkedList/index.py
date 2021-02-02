e061dcf2a8c77a57782dc8045648ed948394ef9f
KhuziamaR <krehman000@citymail.cuny.edu>
2021-02-02 14:42:28 -0500

    sum lists algorithm

--- LinkedList/index.py
+++ LinkedList/index.py
@@ -44,19 +44,49 @@ class LinkedList:
             else:
                 H[cursor.data] = True
             cursor=cursor.next
+    def length(self):
+        cursor = self.head
+        count = 0
+        while cursor:
+            count +=1
+            cursor = cursor.next
+        return count
+    def kth_to_last(self,k):
+        size = self.length()
+        cursor = self.head
+        for i in range(size):
+            if i == size-k:
+                return cursor.data
+            cursor=cursor.next
+def sum_lists(list1,list2):
+    l1 = list1.head
+    l2 = list2.head
+    l3 = LinkedList()
+    carry = 0
+    while l1 or l2:  
+        sum_nodes = l1.data + l2.data + carry
+        carry = 0
+        if sum_nodes >= 10:
+            sum_nodes = sum_nodes %10
+            carry+=1
+            l3.insert(sum_nodes)
+        else:
+            l3.insert(sum_nodes)
+        l1 = l1.next
+        l2 = l2.next
+    return l3
 
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                
+
 data = LinkedList()
-data.head = Node(1)
-# data.insert_head(2)
-# data.insert_head(3)
-# data.insert_head(4)
-# data.insert_head(5)
-# data.insert(0)
-# data.insert(1)
-data.insert(0)
-data.insert(0)
-data.insert(0)
-data.insert(0)
-data.remove_dups()
-data.printLL()
+data.insert(7)
+data.insert(1)
+data.insert(6)
+data2 = LinkedList()
+data2.insert(5)
+data2.insert(9)
+data2.insert(2)
+data3 = sum_lists(data,data2)
+data3.printLL()
+
+
+
